94e7809e292603ca2cce770476695ff57fcc2f8a
johnny <1814607825@qq.com>
2021-06-23 11:21:41 +0800

    调整架构ã

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 function compile(){
     echo ">>>> building $1 <<<<"
-    echo building...
     cd $1/src
     go build -o ../bin/$1
     cd - > /dev/null
+    echo "$1 done >>>>"
 }
 
 function build(){
